9c990ee77c8932eba9df770b5f6a945463a7f754
Ediberto Alves <ediberto@gmail.com>
2023-08-30 19:21:24 -0300

    continuação do exercício Movies - aula 196

--- 14_API_RESTFUL_COM_TS/src/router.ts
+++ 14_API_RESTFUL_COM_TS/src/router.ts
@@ -1,11 +1,7 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/movieControllers";
 
-<<<<<<< HEAD
 import {validate} from "./middleware/handleValidation"
-=======
-import validate from "./middleware/handleValidation"
->>>>>>> 3dd0bb7f07279188b5b3810ba3ff910c5a7b6baf
 
 const router = Router();
 
